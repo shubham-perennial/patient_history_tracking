a3964af74beac32a68e65db942c092d49ae71e48
shubham-Rathod <shubham.rathod@perennialsys.com>
2021-10-29 17:13:48 +0530

    server started and depen added

---
+++ Backend/dist/loaders/server.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = __importDefault(require("../index"));
+index_1.default.listen(2244, async () => {
+    console.log("server is running on 2244 port");
+});
